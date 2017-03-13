6e01d3605eb29f9f1d08b33ac67ccfc91ec418ee
nakitamccool <nakitamccool@gmail.com>
2017-03-13 14:35:45 +0000

    added registration form

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages, auth
 from django.core.urlresolvers import reverse
 from django.shortcuts import render, redirect, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from accounts.forms import UserLoginForm
+from accounts.forms import UserLoginForm, UserRegistrationForm
 from django.template.context_processors import csrf
 
 # Create your views here.
@@ -42,3 +42,27 @@ def login(request):
     args = {'form': form, 'next': request.GET['next'] if request.GET and 'next' in request.GET else ''}
     args.update(csrf(request))
     return render(request, 'login.html', args)
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            user = auth.authenticate(username=request.POST.get('username'), password=request.POST.get('password1'))
+
+            if user:
+                auth.login(request, user)
+                messages.success(request, "You have successfully registered your account")
+                return redirect(reverse('profile'))
+
+            else:
+                messages.error(request, "Unable to log you in at this time")
+
+        else:
+            form = UserRegistrationForm()
+
+        args = {'form': form}
+        args.update(csrf(request))
+
+        return render(request, 'register.html', args)
