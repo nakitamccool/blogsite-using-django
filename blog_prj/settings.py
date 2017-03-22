f3b9a9c2f96e9982456fea00e87cac73c62acc4d
nakitamccool <nakitamccool@gmail.com>
2017-03-22 15:42:27 +0000

    added local host 127.0.0.1 to allowed hosts

--- blog_prj/settings.py
+++ blog_prj/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['nmc17-blog-using-django.herokuapp.com']
+ALLOWED_HOSTS = ['nmc17-blog-using-django.herokuapp.com', '127.0.0.1']
 
 INTERNAL_IPS=['127.0.0.1']
 
